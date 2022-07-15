1f5c924cc4900b7544ae7ec3aaafc402df506226
Bartosz Dorobek <Bartosz.Dorobek@atende.pl>
2022-07-15 13:33:55 +0200

    add variable with versioning

--- main.tf
+++ main.tf
@@ -21,22 +21,28 @@ terraform {
     }
 }
 
+variable "imagebuild" {
+  type        = string
+  description = "Latest Image Build"
+}
+
 resource "azurerm_resource_group" "tf_test" {
   name = "tfmainrg"
   location = "West Europe"
 }
 
-resource "azurerm_container_group" "example" {
+resource "azurerm_container_group" "tfcg_test" {
   name                = "weatherapi"
   location            = azurerm_resource_group.tf_test.location
   resource_group_name = azurerm_resource_group.tf_test.name
+
   ip_address_type     = "Public"
   dns_name_label      = "bdorobek"
   os_type             = "Linux"
 
   container {
     name   = "weatherapi"
-    image  = "bdorobek/weatherapi"
+    image  = "bdorobek/weatherapi:${var.imagebuild}"
     cpu    = "1"
     memory = "1"
 
